f30635cd45753cf82f124f8b3d55b6195a32c20a
Jorge Ivan <59676765+Leviatansan21@users.noreply.github.com>
2020-08-05 14:48:46 -0500

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+echo "Please Wait"
+echo "Deleting Componets"
+echo ""
+rm /usr/share/icons/"Dangerous Creatures"
+rm /usr/share/icons/"Inside your Computer"
+rm /usr/share/icons/"Jungle"
+rm /usr/share/icons/Mystery
+rm /usr/share/icons/BaseBall
+rm /usr/share/icons/"More Windows"
+rm /usr/share/icons/"Leonardo da Vinci"
+rm /usr/share/icons/"Windows 98 Plus"
+rm
