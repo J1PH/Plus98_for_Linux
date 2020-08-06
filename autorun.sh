4f4a9bc48a292794bb1357840ea469e801bca04f
Jorge Ivan <59676765+Leviatansan21@users.noreply.github.com>
2020-08-05 23:28:28 -0500

    Delete autorun.sh

--- autorun.sh
+++
@@ -1,9 +0,0 @@
-echo ""
-echo "Welcome to Microsoft Plus98 for Linux by Leviatan_san"
-echo "======================================================"
-echo ""
-echo Select Opcion
-goto 1 "Install Plu98 for Linux"
-goto 2 "Uninstall Plus98 for Linux"
-goto 3"Exit"
-
